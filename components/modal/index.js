d47b077b6cc8e7548115a1cca2e785015f0641c9
LuisMendoza7 <luismendoza947@hotmail.com>
2018-08-14 16:13:28 -0500

    Added props

--- components/modal/index.js
+++ components/modal/index.js
@@ -16,7 +16,7 @@ import {
   TitleInput,
   DescriptionInput,
   Footer
-} from './styled.js'
+} from './styled'
 
 export class TaskModal extends Component {
   render () {
@@ -25,25 +25,34 @@ export class TaskModal extends Component {
         <Modal
           animationType='fade'
           transparent
-          visible>
+          visible={this.props.isVisible}
+        >
           <KeyboardView behavior='padding'>
             <ModalContainer>
               <TouchableWithoutFeedback onPress={Keyboard.dismiss} accesible={false}>
                 <ModalView>
-                  <Title>Add Task</Title>
+                  <Title>{this.props.title}</Title>
                   <Padding>
                     <Label>Title:</Label>
                     <TitleInput
                       maxLength={25}
                       returnKeyType='next'
                       blurOnSubmit={false}
+                      value={this.props.titleValue}
+                      onChangeText={title => this.props.onChange('title', title)}
+                      onSubmitEditing={() => { this.descriptionInput.root.focus() }}
                     />
                     <Label>Description:</Label>
-                    <DescriptionInput multiline />
+                    <DescriptionInput
+                      multiline
+                      value={this.props.descriptionValue}
+                      onChangeText={description => this.props.onChange('description', description)}
+                      ref={(input) => { this.descriptionInput = input }}
+                    />
                   </Padding>
                   <Footer>
-                    <Button color='black' title='Close' />
-                    <Button color='black' title='Add' />
+                    <Button color='black' title='Close' onPress={this.props.onClose} />
+                    <Button color='black' title={this.props.buttonTitle} onPress={this.props.addTask} />
                   </Footer>
                 </ModalView>
               </TouchableWithoutFeedback>
